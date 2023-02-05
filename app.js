9e3858420f537248e3c91035a57677760712c59f
SouravsGithub <105702695+SouravsGithub@users.noreply.github.com>
2023-02-05 18:25:04 +0530

    added some javascript functionality

---
+++ app.js
@@ -0,0 +1,39 @@
+const notemaker = document.querySelector(".notemaker");
+const addBtn = document.querySelector(".add-btn");
+const modalContainer = document.querySelector(".modal-container");
+const title = document.querySelector("#title");
+const noteBox = document.querySelector("#note-box");
+const ul = document.querySelector(".wrapper");
+const date = new Date();
+const monthsArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+notemaker.addEventListener("click", (event) => {
+    modalContainer.classList.remove("hidden");
+});
+modalContainer.addEventListener("click", (event) => {
+    if (event.target.id === "cross-icon" || event.target.classList.contains("modal-container")) {
+        modalContainer.classList.add("hidden");
+    }
+});
+addBtn.addEventListener("click", () => {
+    if (title.value && noteBox.value) {
+        const day = date.getDate();
+        const year = date.getFullYear();
+        const monthNumber = date.getMonth();
+        let newEl = document.createElement("li");
+        newEl.innerHTML = `<p class="title">${title.value}</p>
+                <div class="note">
+                    <div class="note-text">${noteBox.value}</div>
+                    <div class="bottom-content">
+                        <span class="date">${day}th ${monthsArray[monthNumber]} ${year}</span>
+                        <div class="butons">
+                            <i class="fa-solid fa-ellipsis"></i>
+                        </div>
+                    </div>
+                </div>`;
+        ul.append(newEl);
+        modalContainer.classList.add("hidden");
+        title.value = "";
+        noteBox.value = "";
+    }
+});
+
