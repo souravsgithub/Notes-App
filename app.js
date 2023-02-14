53984cca840c16ddc24d5946d34a30c80ac9a614
SouravsGithub <105702695+SouravsGithub@users.noreply.github.com>
2023-02-14 14:48:22 +0530

    added the delete note functionality

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const notemaker = document.querySelector(".notemaker");
+const smallModal = document.querySelector(".small-modal");
 const addBtn = document.querySelector(".add-btn");
 const modalContainer = document.querySelector(".modal-container");
 const title = document.querySelector("#title");
@@ -14,7 +15,8 @@ modalContainer.addEventListener("click", (event) => {
         modalContainer.classList.add("hidden");
     }
 });
-addBtn.addEventListener("click", () => {
+addBtn.addEventListener("click", (event) => {
+    console.log(event);
     if (title.value && noteBox.value) {
         const day = date.getDate();
         const year = date.getFullYear();
@@ -24,19 +26,19 @@ addBtn.addEventListener("click", () => {
         newEl.innerHTML =
             `
             <div class="top-content">
-            <p class="title">${title.value}</p>
-            <div class="note">
-                <div class="note-text">${noteBox.value}</div>
-            </div>
+                <p class="title">${title.value}</p>
+                <div class="note">
+                    <div class="note-text">${noteBox.value}</div>
+                </div>
             </div>
             <div class="bottom-content">
                 <span class="date">${day}th ${monthsArray[monthNumber]} ${year}</span>
                 <div class="buttons">
+                    <i class="fa-solid fa-ellipsis dots"></i>
                     <div class="small-modal hidden">
                         <a href="" class="edit">Edit</a>
                         <a href="" class="delete">Delete</a>
                     </div>
-                    <i class="fa-solid fa-ellipsis"></i>
                 </div>
             </div>
             `;
@@ -47,3 +49,20 @@ addBtn.addEventListener("click", () => {
     }
 });
 
+document.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.classList.contains("dots")) {
+        event.target.nextElementSibling.classList.toggle("hidden");
+    }
+    if (event.target.classList.contains("delete")) {
+        event.target.closest(".list-items").remove();
+    }
+    if (event.target.classList.contains("edit")) {
+        let oldTitle = event.target.closest(".bottom-content").previousElementSibling.children[0].innerText;
+        let oldParagraph = event.target.closest(".bottom-content").previousElementSibling.children[1].innerText;
+        modalContainer.classList.remove("hidden");
+        title.value = oldTitle;
+        noteBox.value = oldParagraph;
+        event.target.closest(".list-items").remove();
+    }
+});
